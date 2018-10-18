1b7d2a6a11492d89c4003623e044e943dfa9ff0b
tproho1 <tproho1@etf.unsa.ba>
2018-10-18 12:24:04 +0200

    T1Z3 Prekucavanje cpp koda u Java kod

---
+++ src/etf/unsa/ba/Sat.java
@@ -0,0 +1,61 @@
+package etf.unsa.ba;
+import java.util.Scanner;
+
+public class Sat {
+    static int sati=0;
+    static int minute=0;
+    static int sekunde=0;
+
+
+    Sat(int sati, int minute, int sekunde){
+        Postavi(sati, minute, sekunde);
+    }
+
+    public void Postavi(int sati, int minute, int sekunde){
+        sati=sati;
+        minute=minute;
+        sekunde=sekunde;
+    }
+
+    public void Sljedeci(){
+        sekunde++;
+        if(sekunde==60){
+            sekunde=0; minute++;
+        }
+        if(minute==60){
+            minute=0; sati++;
+        }
+        if(sati==24) sati=0;
+    }
+
+
+    public void Prethodni(){
+        sekunde--;
+        if(sekunde==-1){
+            sekunde=59; minute--;
+        }
+        if(minute==-1){
+            minute=0; sati--;
+        }
+        if(sati==-1) sati=23;
+    }
+
+    public void PomjeriZa(int pomak){
+        if(pomak>0) for(int i=0; i<pomak; i++) Sljedeci();
+        else for(int i=0; i<-pomak; i++) Prethodni();
+    }
+
+    public int GetSate()  {return sati;}
+    public int GetMinute()  {return minute;}
+    public int GetSekunde() {return sekunde;}
+
+    public void Ispisi() {
+        System.out.println(sati + ":" + minute + ":" + sekunde);
+    }
+
+
+    public static void main(String[] args) {
+	    Scanner ulaz = new Scanner (System.in);
+
+    }
+}
